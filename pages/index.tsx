db83cd724986cdad68752efd664d4bf6eda834cc
afp2 <afp2@cin.ufpe.br>
2025-04-29 20:35:21 -0300

    feat: atualização no visual de h2 e p

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className='font-roboto'>
       <Header />
       <main className='px-[20px] py-[60px] text-center'>
-        <h2 className='font-semibold text-size-[4rem]'>Bem-vindo à NEPCOM</h2>
+        <h2 className='font-semibold text-size-[4rem]'>Bem-vindo ao NEPCOM</h2>
         <p className='mt-[20px]'>Somos uma equipe de desenvolvimento em TI, freelancing e soluções tecnológicas.</p>
       </main>
     </div>
