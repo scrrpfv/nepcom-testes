dfea81ea244ed11fad9dd8caffa6ae6338cf9ce1
afp2 <afp2@cin.ufpe.br>
2025-05-12 21:42:51 -0300

    fix: adicionado novo parágrafo no texto do contato

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
       <Header />
       <main className='px-[20px] py-[60px]'>
         <h1 className="font-semibold text-size-[4rem]">Entre em Contato</h1>
-        <p className='mt-[20px]'>Estamos prontos para ouvir suas ideias, tirar dúvidas e ajudar a transformar desafios em soluções tecnológicas eficientes.
+        <p className='mt-[20px]'>Estamos prontos para ouvir suas ideias, tirar dúvidas e ajudar a transformar desafios em soluções tecnológicas eficientes.<br /><br />
 Se você tem um projeto em mente, precisa de um site, sistema ou consultoria em TI, ou simplesmente quer saber mais sobre o nosso trabalho, fale com a gente!
 Nossa equipe está sempre aberta a novas conexões e parcerias..</p>
         <form
